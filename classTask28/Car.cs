29ae0855dff06919720ae429ada125c94edb88e1
FeridQarayev <tu7he6n9s@code.edu.az>
2022-11-28 08:46:25 +0400

    Task28

---
+++ classTask28/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTask28
+{
+    internal class Car
+    {
+        int _doorCount;
+        string _pedalKind;
+        double _wingLength;
+
+
+        private int Count;
+
+        public int DoorCount
+        {
+            get
+            {
+                return _doorCount;
+            }
+            set
+            {
+                _doorCount = value;
+            }
+        }
+
+        private string Pedal;
+
+        public string PedalKind
+        {
+            get { return PedalKind; }
+            set { PedalKind = value; }
+        }
+
+    }
+    private int _wingLength;
+
+    public int MyProperty
+    {
+        get { return _wingLength; }
+        set { _wingLength = value; }
+    }
+
+
+
+}
---
+++ classTask28/VecileClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classTask28
+{
+    internal abstract class VecileClass
+    {
+        double _driveTime;
+        double _drivePath;
+
+
+        private double Drive;
+
+
+        public double DriveTime
+        {
+            get
+            {
+                return _driveTime;
+            }
+            set
+            {
+                if(value>0)
+                _driveTime = value; 
+            }
+        }
+
+
+        private double Path;
+
+        public double DrivePath
+        {
+            get
+            {
+                return DrivePath;
+            }
+            set 
+            {
+                if (value > 0)
+                    DrivePath = value;
+            }
+        }
+        public void AverageSpeed()
+        {
+            Console.WriteLine(driveTime + path) / 2;
+        }
+    }
+
+    
+}
+
+
+  
